1bcc0ff3ae40798c8dc5e8212ebdaec37cad1424
Thrirry <tam300497@gmail.com>
2017-12-22 15:05:05 +0700

    Update(Last/22/12)

--- src/main/java/com/mazuz/repositories/OrderedRepository.java
+++ src/main/java/com/mazuz/repositories/OrderedRepository.java
@@ -1,7 +1,6 @@
 package com.mazuz.repositories;
 
 import com.mazuz.domain.Ordered;
-import com.sun.istack.internal.Nullable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
